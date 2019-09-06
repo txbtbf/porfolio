fbd092e6264ce44126b570143539eaba5a07c834
Tom <txbtbf@Gmail.com>
2019-09-06 12:58:20 -0400

    Initial Commit

---
+++ src/app/shared/post.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from "@angular/core";
+
+import {TableModule} from "primeng/table";
+import { CalendarModule } from "primeng/components/calendar/calendar";
+
+import { SharedModule } from "./shared.module";
+
+import { CommentTableComponent } from "../manage/content-mng/comment-table/comment-table.component";
+import { PostTableComponent } from "../manage/content-mng/post-table/post-table.component";
+
+@NgModule({
+  imports: [
+    SharedModule,
+    TableModule,
+    CalendarModule
+  ],
+  declarations: [
+    CommentTableComponent,
+    PostTableComponent
+  ],
+  exports: [
+    CommentTableComponent,
+    PostTableComponent
+  ]
+})
+
+export class PostSharedModule {
+
+}
